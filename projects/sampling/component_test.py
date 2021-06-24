ea45c9c5fe636ae8971b9847cea41ccadff62c84
Daniel Kim <danielk@twitter.com>
2021-06-24 14:26:48 -0400

    Add basic component tests

---
+++ projects/sampling/component_test.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import tensorflow_data_validation as tfdv
+import component
+from tfx.types import artifact_utils
+from tfx.types import channel_utils
+from tfx.types import standard_artifacts
+
+INPUT_KEY = 'input_data'
+OUTPUT_KEY = 'output_data'
+LABEL_KEY = 'label'
+NAME_KEY = 'name'
+SPLIT_KEY = 'splits'
+COPY_KEY = 'copy_others'
+SHARDS_KEY = 'shards'
+
+class ComponentTest(tf.test.TestCase):
+  def testConstruct(self):
+    examples = standard_artifacts.Examples()
+    examples.split_names = artifact_utils.encode_split_names(['train', 'eval'])
+    
+    under = component.UndersamplingComponent(
+        input_data=channel_utils.as_channel([examples]),
+        label='label')
+
+    self.assertEqual(
+        standard_artifacts.Examples.TYPE_NAME, under.outputs[OUTPUT_KEY].type_name)
+    self.assertEqual(
+        under.spec.exec_properties[SPLIT_KEY], ['train'])
+    self.assertEqual(
+        under.spec.exec_properties[LABEL_KEY], 'label')
+
+  def testConstructWithOptions(self):
+    examples = standard_artifacts.Examples()
+    examples.split_names = artifact_utils.encode_split_names(['train', 'eval'])
+
+    under = component.UndersamplingComponent(
+        input_data=channel_utils.as_channel([examples]),
+        label='test_label',
+        name='test_name',
+        splits=['train', 'eval'],
+        copy_others=False,
+        shards=10)
+
+    self.assertEqual(
+        standard_artifacts.Examples.TYPE_NAME, under.outputs[OUTPUT_KEY].type_name)
+    self.assertEqual(
+        under.spec.exec_properties[LABEL_KEY], 'test_label')
+    self.assertEqual(
+        under.spec.exec_properties[NAME_KEY], 'test_name')
+    self.assertEqual(
+        under.spec.exec_properties[SPLIT_KEY], ['train', 'eval'])
+    self.assertEqual(
+        under.spec.exec_properties[COPY_KEY], False)
+    self.assertEqual(
+        under.spec.exec_properties[SHARDS_KEY], 10)
+
+def run_tests():
+  t = ComponentTest()
+  t.testConstruct()
+  t.testConstructWithOptions()
+
+if __name__ == '__main__':
+  run_tests()
