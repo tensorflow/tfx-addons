ba4784cd29a697c7d104d0ec80b0d6b857ab3b61
Daniel Kim <danielk@twitter.com>
2021-07-21 12:25:15 -0400

    Add spec file with spec and labels

---
+++ projects/sampler/spec.py
@@ -0,0 +1,42 @@
+"""Sampling component definition."""
+
+from typing import Optional, Text, List
+from sampler.executor import Executor
+
+from tfx import types
+from tfx.dsl.components.base import base_beam_component
+from tfx.dsl.components.base import executor_spec
+from tfx.types import channel_utils
+from tfx.types import standard_artifacts
+from tfx.types.component_spec import ChannelParameter
+from tfx.types.component_spec import ExecutionParameter
+from tfx.utils import json_utils
+
+SAMPLER_INPUT_KEY = 'input_data'
+SAMPLER_OUTPUT_KEY = 'output_data'
+SAMPLER_LABEL_KEY = 'label'
+SAMPLER_NAME_KEY = 'name'
+SAMPLER_SPLIT_KEY = 'splits'
+SAMPLER_COPY_KEY = 'copy_others'
+SAMPLER_SHARDS_KEY = 'shards'
+SAMPLER_CLASSES_KEY = 'keep_classes'
+SAMPLER_SAMPLE_KEY = 'undersample'
+
+class SamplerSpec(types.ComponentSpec):
+  """Sampling component spec."""
+
+  PARAMETERS = {
+    SAMPLER_LABEL_KEY: ExecutionParameter(type=str),
+    SAMPLER_NAME_KEY: ExecutionParameter(type=Text, optional=True),
+    SAMPLER_SPLIT_KEY: ExecutionParameter(type=str, optional=True),
+    SAMPLER_COPY_KEY: ExecutionParameter(type=int, optional=True),
+    SAMPLER_SHARDS_KEY: ExecutionParameter(type=int, optional=True),
+    SAMPLER_CLASSES_KEY: ExecutionParameter(type=str, optional=True),
+    SAMPLER_SAMPLE_KEY: ExecutionParameter(type=int, optional=True)
+  }
+  INPUTS = {
+    SAMPLER_INPUT_KEY: ChannelParameter(type=standard_artifacts.Examples),
+  }
+  OUTPUTS = {
+    SAMPLER_OUTPUT_KEY: ChannelParameter(type=standard_artifacts.Examples),
+  }
