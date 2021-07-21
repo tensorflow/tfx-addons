78fe3e25c7ea38a734996d846009f39e4b28cd78
Daniel Kim <danielk@twitter.com>
2021-07-21 12:25:36 -0400

    Refactor executor with new changes and fix some merge errors

--- projects/sampler/executor.py
+++ projects/sampler/executor.py
@@ -6,6 +6,8 @@ import random
 import apache_beam as beam
 from typing import Any, Dict, List, Text
 
+import spec
+
 from tfx import types
 from tfx.dsl.components.base import base_beam_executor
 from tfx.dsl.io import fileio
@@ -16,8 +18,7 @@ from tfx.utils import io_utils
 class Executor(base_beam_executor.BaseBeamExecutor):
   """Executor for Sampler."""
 
-  def _CreatePipeline(
-      self, unused_transform_output_path: Text) -> beam.Pipeline:
+  def _CreatePipeline(self, unused_transform_output_path: Text) -> beam.Pipeline:
     """Creates beam pipeline.
     Args:
       unused_transform_output_path: unused.
@@ -62,15 +63,16 @@ class Executor(base_beam_executor.BaseBeamExecutor):
     """
 
     self._log_startup(input_dict, output_dict, exec_properties)
-    label = exec_properties['label']
-    undersample = exec_properties['undersample']
-    splits = json_utils.loads(exec_properties['splits'])
-    copy_others = exec_properties['copy_others']
-    shards = exec_properties['shards']
-    keep_classes = json_utils.loads(exec_properties['keep_classes'])
 
-    input_artifact = artifact_utils.get_single_instance(input_dict['input_data'])
-    output_artifact = artifact_utils.get_single_instance(output_dict['output_data'])
+    label = exec_properties[spec.SAMPLER_LABEL_KEY]
+    undersample = exec_properties[spec.SAMPLER_SAMPLE_KEY]
+    splits = json_utils.loads(exec_properties[spec.SAMPLER_SPLIT_KEY])
+    copy_others = exec_properties[spec.SAMPLER_COPY_KEY]
+    shards = exec_properties[spec.SAMPLER_SHARDS_KEY]
+    keep_classes = json_utils.loads(exec_properties[spec.SAMPLER_CLASSES_KEY])
+
+    input_artifact = artifact_utils.get_single_instance(input_dict[spec.SAMPLER_INPUT_KEY])
+    output_artifact = artifact_utils.get_single_instance(output_dict[spec.SAMPLER_OUTPUT_KEY])
 
     if copy_others:
       output_artifact.split_names = input_artifact.split_names
@@ -86,8 +88,9 @@ class Executor(base_beam_executor.BaseBeamExecutor):
     for split, uri in split_data.items():
       if split in splits:  # Undersampling split
         output_dir = artifact_utils.get_split_uri([output_artifact], split)
-        os.mkdir(output_dir)
-        _undersample(uri, label, shards, keep_classes, os.path.join(output_dir, f"Split-{split}"))
+        split_dir = os.path.join(output_dir, f"Split-{split}")
+        with self._CreatePipeline(split_dir) as p:
+          _sample(p, uri, label, shards, keep_classes, split_dir, undersample)
       elif copy_others:  # Copy the other split if copy_others is True
         input_dir = uri
         output_dir = artifact_utils.get_split_uri([output_artifact], split)
@@ -110,8 +113,9 @@ def _generate_elements(x, label):
       class_label = val[0].decode()
   return (class_label, parsed)
 
-def _sample_data(key, val, side=0):
-  for item in random.sample(val, side):
+def _sample_data(key, val, undersample=True, side=0):
+  random_sample_data = random.sample(val, side) if undersample else random.choices(val, k=side)
+  for item in random_sample_data:
     yield item
 
 def _filter_null(item, keep_null=False, null_vals=None):
@@ -126,7 +130,7 @@ def _filter_null(item, keep_null=False, null_vals=None):
   if keep:
     return item
 
-def _undersample(uri, label, shards, keep_classes, output_dir):
+def _sample(p, uri, label, shards, keep_classes, output_dir, undersample):
   """Function that actually undersamples the given split.
 
   Args:
@@ -142,14 +146,12 @@ def _undersample(uri, label, shards, keep_classes, output_dir):
     None
   """
 
-  with beam.Pipeline() as p:
-    data = _read_tfexamples(p, uri, label)
-    merged = _sample_examples(p, data, keep_classes)
-    _write_tfexamples(p, merged, shards, output_dir)
+  data = _read_tfexamples(p, uri, label)
+  merged = _sample_examples(p, data, keep_classes, undersample)
+  _write_tfexamples(p, merged, shards, output_dir)
 
 def _read_tfexamples(p, uri, label):
-  files = [os.path.join(uri, name) for name in os.listdir(uri)]
-  dataset = tf.data.TFRecordDataset(files, compression_type="GZIP")
+  dataset = tf.data.TFRecordDataset(tf.data.Dataset.list_files(f'{uri}/*'), compression_type="GZIP")
   
   # Take the input TFRecordDataset and extract the class label that we want.
   # Output format is a K-V PCollection: {class_label: TFRecord in string format}
@@ -160,15 +162,22 @@ def _read_tfexamples(p, uri, label):
   )
   return data
 
-def _sample_examples(p, data, keep_classes):
+def _sample_examples(p, data, keep_classes, undersample):
     # Finds the minimum frequency of all classes in the input label.
     # Output is a singleton PCollection with the minimum # of examples.
+
+  def find_minimum(elements):
+    return min(elements or [0])
+  def find_maximum(elements):
+    return max(elements or [0])
+  sample_fn = find_minimum if undersample else find_maximum
+
   val = (
       data
       | "CountPerKey" >> beam.combiners.Count.PerKey()
       | "FilterNullCount" >> beam.Filter(lambda x: _filter_null(x, null_vals=keep_classes))
       | "Values" >> beam.Values()
-      | "FindMinimum" >> beam.CombineGlobally(lambda elements: min(elements or [-1]))
+      | "GetSample" >> beam.CombineGlobally(sample_fn)
     )
 
   # Actually performs the undersampling functionality.
@@ -177,7 +186,7 @@ def _sample_examples(p, data, keep_classes):
     data
     | "GroupBylabel" >> beam.GroupByKey()
     | "FilterNull" >> beam.Filter(lambda x: _filter_null(x, null_vals=keep_classes))
-    | "Undersample" >> beam.FlatMapTuple(_sample_data, side=beam.pvalue.AsSingleton(val))
+    | "Undersample" >> beam.FlatMapTuple(_sample_data, undersample, side=beam.pvalue.AsSingleton(val))
   )
 
   # Take out all the null values from the beginning and put them back in the pipeline
