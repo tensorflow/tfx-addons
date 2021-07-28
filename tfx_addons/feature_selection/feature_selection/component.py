f1860978d58582bbf049c54dd636ad685829dcd2
deutranium <29124655+deutranium@users.noreply.github.com>
2021-07-29 04:33:54 +0530

    Updated component with selected data and features

--- tfx_addons/feature_selection/feature_selection/component.py
+++ tfx_addons/feature_selection/feature_selection/component.py
@@ -24,10 +24,12 @@ from module_file import _FEATURE_KEYS, _NUM_PARAM, _INPUT_DATA, _TARGET_DATA, Se
 
 class FeatureSelectionArtifact(artifact.Artifact):
   """Output artifact containing feature scores from the Feature Selection component"""
-  TYPE_NAME = 'Feature Scores and P-Values'
+  TYPE_NAME = 'Feature Selection'
   PROPERTIES = {
       'scores': artifact.Property(type=artifact.PropertyType.JSON_VALUE),
-      'p_values': artifact.Property(type=artifact.PropertyType.JSON_VALUE)
+      'p_values': artifact.Property(type=artifact.PropertyType.JSON_VALUE),
+      'selected_features': artifact.Property(type=artifact.PropertyType.JSON_VALUE),
+      'selected_data': artifact.Property(type=artifact.PropertyType.JSON_VALUE)
   }
 
 
@@ -37,19 +39,14 @@ Feature selection component using sklearn
 
 @component
 def FeatureSelection(feature_selection: tfx.dsl.components.OutputArtifact[FeatureSelectionArtifact]):
-  """Feature Selection component
-    Args:
-    input_data: Input features in the form of list[list] where each inner list contains one record
-    target_column: The target column which is inferred from `input_data`
-    selector_func: feature selector type
-    score_func: score function for feature selection. Example: chi2 etc.
-    num_params: Parameter of the corresponding `selector_func`
-    column_names: to generate artifact dictionaries containing scores
-    """
+  """Feature Selection component"""
 
   # Select features based on scores
   selector = SelectorFunc(ScoreFunc, k=_NUM_PARAM)
-  selected_data = selector.fit_transform(_INPUT_DATA, _TARGET_DATA)
+  selected_data = selector.fit_transform(_INPUT_DATA, _TARGET_DATA).tolist()
+
+  # generate a list of selected features by matching _FEATURE_KEYS to selected indices
+  selected_features = [val for (idx, val) in enumerate(_FEATURE_KEYS) if idx in selector.get_support(indices=True)]
 
   # get scores and p-values for artifacts
   selector_scores = selector.scores_
@@ -59,5 +56,8 @@ def FeatureSelection(feature_selection: tfx.dsl.components.OutputArtifact[Featur
   selector_scores_dict = dict(zip(_FEATURE_KEYS, selector_scores))
   selector_pvalues_dict = dict(zip(_FEATURE_KEYS, selector_p_values))
 
-  feature_selection.set_json_value_custom_property("scores", selector_scores_dict)
-  feature_selection.set_json_value_custom_property("pvalues", selector_pvalues_dict)
+  # populate artifact with the required properties
+  feature_selection.scores = selector_scores_dict
+  feature_selection.p_values = selector_pvalues_dict
+  feature_selection.selected_features = selected_features
+  feature_selection.selected_data = selected_data
