564964d367d2f0bc6d25ac002d89b49ab23a86a1
deutranium <29124655+deutranium@users.noreply.github.com>
2021-07-28 10:22:29 +0530

    Modified component to get values from the module file

--- tfx_addons/feature_selection/feature_selection/component.py
+++ tfx_addons/feature_selection/feature_selection/component.py
@@ -14,68 +14,62 @@
 # ==============================================================================
 
 from typing import List
-
 import tfx.v1 as tfx
-from sklearn.feature_selection import (SelectFdr, SelectFpr, SelectFwe,
-                                       SelectKBest, SelectPercentile)
 from tfx.dsl.component.experimental.decorators import component
-from tfx.dsl.components import Parameter, OutputArtifact
-from tfx.types import artifact
+from tfx.types import artifact, standard_artifacts
 
-_selection_modes = {
-    'percentile': SelectPercentile,
-    'k_best': SelectKBest,
-    'fpr': SelectFpr,
-    'fdr': SelectFdr,
-    'fwe': SelectFwe
-}
+from module_file import _FEATURE_KEYS, _NUM_PARAM, _INPUT_DATA, _TARGET_DATA, SelectorFunc, ScoreFunc
+
+# _selection_modes = {
+#   'percentile': SelectPercentile,
+#   'k_best': SelectKBest,
+#   'fpr': SelectFpr,
+#   'fdr': SelectFdr,
+#   'fwe': SelectFwe
+# }
+
 """Custom Artifact type"""
 
-
-class FeatureSelection(artifact.Artifact):
+class FeatureSelectionArtifact(artifact.Artifact):
   """Output artifact containing feature scores from the Feature Selection component"""
   TYPE_NAME = 'Feature Scores'
   PROPERTIES = {
-      'scores': {},
-      'p_values': {}
+    'scores': {},
+    'p_values': {}
   }
-
 
 
 """
 Feature selection component using sklearn
 """
-@component
-def FeatureSelection(input_data: List,
-                     target_column: List,
-                     feature_selection: OutputArtifact[FeatureSelection],
-                     selector_func,
-                     score_func,
-                     column_names=[],
-                     num_param: Parameter[int] = 10,
-                     ) -> None:
-  """Feature Selection component
 
+
+@component
+def FeatureSelection(feature_selection: tfx.dsl.components.OutputArtifact[FeatureSelectionArtifact]):
+  """Feature Selection component
     Args:
-        input_data: Input features in the form of list[list] where each inner list contains one record
-        target_column: The target column which is inferred from `input_data`
-        selector_func: feature selector type
-        score_func: score function for feature selection. Example: chi2 etc.
-        num_params: Parameter of the corresponding `selector_func`
-        column_names: to generate artifact dictionaries containing scores
+    input_data: Input features in the form of list[list] where each inner list contains one record
+      target_column: The target column which is inferred from `input_data`
+      selector_func: feature selector type
+      score_func: score function for feature selection. Example: chi2 etc.
+      num_params: Parameter of the corresponding `selector_func`
+      column_names: to generate artifact dictionaries containing scores
     """
 
   # Select features based on scores
-  selector = _selection_modes[selector_func](score_func, k=num_param)
-  selected_data = selector.fit_transform(input_data, target_column)
+  selector = SelectorFunc(ScoreFunc, k=_NUM_PARAM)
+  # selected_data = selector.fit_transform(_INPUT_DATA, _TARGET_DATA)
 
   # get scores and p-values for artifacts
   selector_scores = selector.scores_
   selector_p_values = selector.pvalues_
+  print("!!!!!!!", selector_scores)
 
-  # convert scores and p-values to dictionaries with column names as keys for better comprehensibility
-  # add the dictionaries to the artifact
-  feature_selection.scores = dict(zip(column_names, selector_scores))
-  feature_selection.pvalues = dict(zip(column_names, selector_p_values))
+  # merge scores and pvalues with feature keys to create a dictionary
+  selector_scores_dict = dict(zip(_FEATURE_KEYS, selector_scores))
+  selector_pvalues_dict = dict(zip(_FEATURE_KEYS, selector_p_values))
+  print("!!!!!!!", selector_scores_dict)
+  print("!!!!!!!", selector_pvalues_dict)
 
-  return selected_data
+  feature_selection.set_json_value_custom_property("scores", selector_scores_dict)
+  feature_selection.set_json_value_custom_property("pvalues", selector_pvalues_dict)
--- tfx_addons/feature_selection/feature_selection/module_file.py
+++ tfx_addons/feature_selection/feature_selection/module_file.py
@@ -1,10 +1,25 @@
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from sklearn.datasets import load_iris
 from sklearn.feature_selection import SelectKBest as SelectorFunc
 from sklearn.feature_selection import chi2 as ScoreFunc
-from sklearn.datasets import load_iris
 
 iris_data = load_iris()
-_FEATURE_KEYS = load_iris.feature_names
+_FEATURE_KEYS = iris_data.feature_names
 _INPUT_DATA = iris_data.data
 _TARGET_DATA = iris_data.target
 
-_NUM_PARAM = 10
+_NUM_PARAM = 2
