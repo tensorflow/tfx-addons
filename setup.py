2f1193562baa9c815a044216490ba6b896c87fb9
cent5 <vincentn@twitter.com>
2021-08-25 20:55:11 -0700

    readd package ci for tfx 0.30.2

--- setup.py
+++ setup.py
@@ -30,7 +30,10 @@ TESTS_REQUIRE = ["pytest", "pylint", "pre-commit", "isort", "yapf"]
 
 EXTRAS_REQUIRE = {
     # Add dependencies here for your project. Avoid using install_requires.
-    "mlmd_client": ["ml-pipelines-sdk>=1.0.0<2", "ml-metadata>=1.0.0<2"],
+    "mlmd_client": [
+        "ml-pipelines-sdk>=0.26.3<2.0.0",
+        "ml-metadata>=0.26<2.0.0",
+    ],
     "schema_curation": [
         "tfx>=0.26.3<2.0.0",
     ],
