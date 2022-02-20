b1dfe445dbfcc6964b8b8c23615cd08537e51724
Hannes Hapke <hannes.hapke@gmail.com>
2022-02-20 13:32:14 -0800

    exclude lines from pylint

--- tfx_addons/message_exit_handler/proto/slack_pb2.py
+++ tfx_addons/message_exit_handler/proto/slack_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x0bslack.proto\x12\x1dtfx_addons.slack_exit_handler\":\n\tSlackSpec\x12\x13\n\x0bslack_token\x18\x01 \x01(\t\x12\x18\n\x10slack_channel_id\x18\x02 \x01(\tb\x06proto3'
+  serialized_pb=b'\n\x0bslack.proto\x12\x1dtfx_addons.slack_exit_handler\":\n\tSlackSpec\x12\x13\n\x0bslack_token\x18\x01 \x01(\t\x12\x18\n\x10slack_channel_id\x18\x02 \x01(\tb\x06proto3'  # pylint: disable=line-too-long
 )
 
 
@@ -31,22 +31,22 @@ _SLACKSPEC = _descriptor.Descriptor(
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
-  create_key=_descriptor._internal_create_key,
+  create_key=_descriptor._internal_create_key,  # pylint: disable=protected-access
   fields=[
     _descriptor.FieldDescriptor(
-      name='slack_token', full_name='tfx_addons.slack_exit_handler.SlackSpec.slack_token', index=0,
+      name='slack_token', full_name='tfx_addons.slack_exit_handler.SlackSpec.slack_token', index=0,  # pylint: disable=line-too-long
       number=1, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),  # pylint: disable=line-too-long
     _descriptor.FieldDescriptor(
-      name='slack_channel_id', full_name='tfx_addons.slack_exit_handler.SlackSpec.slack_channel_id', index=1,
+      name='slack_channel_id', full_name='tfx_addons.slack_exit_handler.SlackSpec.slack_channel_id', index=1,  # pylint: disable=line-too-long
       number=2, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),  # pylint: disable=protected-access
   ],
   extensions=[
   ],
