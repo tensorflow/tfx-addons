9f49afb169678577923bbd28338d75056fff8930
deutranium <29124655+deutranium@users.noreply.github.com>
2022-08-21 02:33:47 +0530

    Added test to check if correct features are being selected

--- tfx_addons/feature_selection/component_test.py
+++ tfx_addons/feature_selection/component_test.py
@@ -14,6 +14,8 @@
 # ==============================================================================
 """Tests for tfx_addons.feature_selection.component"""
 
+import csv
+import importlib
 import os
 from typing import List, Optional, Text
 
@@ -24,6 +26,39 @@ from tfx.orchestration import metadata
 from tfx_addons.feature_selection import component
 
 
+def _get_selected_features(module_file, data_path):
+  """Get the correct selected features for testing"""
+
+  data = []
+
+  # importing required configurations
+  modules = importlib.import_module(module_file)
+  mod_names = ["SELECTOR_PARAMS", "TARGET_FEATURE", "SelectorFunc"]
+  selector_params, target_feature, selector_func = [
+      getattr(modules, i) for i in mod_names
+  ]
+
+  # getting the data
+  with open(data_path, 'r') as file:
+    my_reader = csv.reader(file, delimiter=',')
+    for row in my_reader:
+      data.append(row)
+
+  # splitting X (input) and Y (output) from CSV data
+  target_idx = data[0].index(target_feature)
+  target_data = [i.pop(target_idx) for i in data]
+
+  # runnign the selector function for feature selection
+  selector = selector_func(**selector_params)
+  selector.fit_transform(data[1:], target_data[1:])
+
+  # getting selected feature names
+  selected_indices = selector.get_support(indices=True)
+  final_features = set(data[0][idx] for idx in selected_indices)
+
+  return final_features
+
+
 def _create_pipeline(
     pipeline_name: Text,
     pipeline_root: Text,
@@ -31,10 +66,11 @@ def _create_pipeline(
     module_path: Text,
     metadata_path: Text,
     beam_pipeline_args: Optional[List[Text]] = None) -> tfx.v1.dsl.Pipeline:
+  """Creating sample pipeline with two components: CsvExampleGen and
+  FeatureSelection"""
 
+  # specifying the pipeline components
   example_gen = tfx.components.CsvExampleGen(input_base=data_root)
-
-  # creating and executing the FeatureSelection artifact
   feature_selection = component.FeatureSelection(
       orig_examples=example_gen.outputs['examples'], module_path=module_path)
 
@@ -56,11 +92,14 @@ class FeatureSelectionTest(tf.test.TestCase):
         os.environ.get('TEST_UNDECLARED_OUTPUTS_DIR', self.get_temp_dir()),
         self._testMethodName)
     self._feature_selection_root = os.path.dirname(__file__)
-
     self._pipeline_name = 'feature_selection'
     self._data_root = os.path.join(self._feature_selection_root, 'test')
+    self._data_path = os.path.join(self._data_root, 'iris.csv')
     self._module_path = os.path.join(self._feature_selection_root, 'example',
                                      'modules', 'iris_module_file.py')
+    self._module_file = "tfx_addons.feature_selection.example.modules.\
+iris_module_file"
+
     self._pipeline_root = os.path.join(self._test_dir, 'tfx', 'pipelines',
                                        self._pipeline_name)
     self._metadata_path = os.path.join(self._test_dir, 'tfx', 'metadata',
@@ -87,19 +126,35 @@ class FeatureSelectionTest(tf.test.TestCase):
                          module_path=self._module_path,
                          metadata_path=self._metadata_path))
 
-    expected_execution_count = 2
+    expected_execution_count = 2  # one each for CsvExampleGen and Feature Selection
+    true_selected_features = _get_selected_features(self._module_file,
+                                                    self._data_path)
 
     metadata_config = (
         tfx.orchestration.metadata.sqlite_metadata_connection_config(
             self._metadata_path))
     with metadata.Metadata(metadata_config) as m:
-      artifact_count = len(m.store.get_artifacts())
       execution_count = len(m.store.get_executions())
+      selected_features_struct = list(
+          m.store.get_artifacts_by_type(
+              "Feature Selection")[0].properties["selected_features"].
+          struct_value.fields.values.__self__["__value__"].list_value.values)
+      component_selected_features = set(
+          feature.string_value for feature in selected_features_struct)
+
+      # TEST: execution count
+      self.assertEqual(expected_execution_count, execution_count)
+
+      # TEST: number of artifacts with TYPE_NAME `Feature Selection`
       self.assertEqual(1,
                        len(m.store.get_artifacts_by_type("Feature Selection")))
+
+      # TEST: number of artifacts with TYPE_NAME `Examples`
+      # (one each from CsvExampleGen and FeatureSelection)
       self.assertEqual(2, len(m.store.get_artifacts_by_type("Examples")))
-      self.assertGreaterEqual(artifact_count, execution_count)
-      self.assertEqual(expected_execution_count, execution_count)
+
+      # TEST: if the features selected by component are correct
+      self.assertEqual(component_selected_features, true_selected_features)
 
     self.assertPipelineExecution()
 
