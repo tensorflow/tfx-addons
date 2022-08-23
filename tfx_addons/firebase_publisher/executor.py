1dc08358120192b6b7a381c5b8826eb2140fd327
Chansung Park <deep.diver.csp@gmail.com>
2022-08-23 08:05:11 +0000

    unncessary codes from custom_config

--- tfx_addons/firebase_publisher/executor.py
+++ tfx_addons/firebase_publisher/executor.py
@@ -9,10 +9,10 @@ from tfx.utils import name_utils
 from tfx.utils import telemetry_utils
 from tfx.dsl.io import fileio
 
-from tfx.components.pusher import executor as tfx_pusher_executor
+from tfx.dsl.components.base import base_executor
 from tfx_addons.firebase_publisher import runner
 
-class Executor(tfx_pusher_executor.Executor):
+class Executor(base_executor.BaseExecutor):
     """Push a model to Firebase ML"""
 
     def Do(
@@ -36,18 +36,6 @@ class Executor(tfx_pusher_executor.Executor):
         """
         self._log_startup(input_dict, output_dict, exec_properties)
 
-        custom_config = json_utils.loads(
-            exec_properties.get(_CUSTOM_CONFIG_KEY, "null")
-        )
-
-        if custom_config is not None and not isinstance(custom_config, Dict):
-            raise ValueError(
-                "custom_config in execution properties needs to be a dict."
-            )
-
-        firebase_ml_args = custom_config.get(constants.FIREBASE_ML_KEY)
-        if not firebase_ml_args:
-            raise ValueError("'FIREBASE_ML' is missing in 'custom_config'")
         model_push = artifact_utils.get_single_instance(
             output_dict[standard_component_specs.PUSHED_MODEL_KEY]
         )
@@ -58,12 +46,6 @@ class Executor(tfx_pusher_executor.Executor):
         # Deploy the model.
         io_utils.copy_dir(src=self.GetModelPath(input_dict), dst=model_push.uri)
         model_path = model_push.uri
-
-        executor_class_path = name_utils.get_full_name(self.__class__)
-        with telemetry_utils.scoped_labels(
-            {telemetry_utils.LABEL_TFX_EXECUTOR: executor_class_path}
-        ):
-            job_labels = telemetry_utils.make_labels_dict()
 
         model_name = f"v{int(time.time())}"
         pushed_model_path = runner.deploy_model_for_firebase_ml(
