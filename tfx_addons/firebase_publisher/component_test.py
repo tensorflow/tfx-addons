40ccf934aa0f57fdbac26485e82f168da424bc17
Chansung Park <deep.diver.csp@gmail.com>
2022-08-26 02:13:00 +0000

    add basic unit tests

--- tfx_addons/firebase_publisher/component_test.py
+++ tfx_addons/firebase_publisher/component_test.py
@@ -8,13 +8,12 @@ from tfx_addons.firebase_publisher.component import FirebasePublisher
 
 class FirebasePublisherTest(tf.test.TestCase):
   def testConstruct(self):
-    firebase_publisher = FirebasePublisher(
-        display_name="test_display_name",
-        storage_bucket="storage_bucket"
-    )
+    firebase_publisher = FirebasePublisher(display_name="test_display_name",
+                                           storage_bucket="storage_bucket")
 
     self.assertEqual(standard_artifacts.PushedModel.TYPE_NAME,
                      firebase_publisher.outputs['pushed_model'].type_name)
+
 
 if __name__ == '__main__':
   tf.test.main()
