68e488dd4edfa2c55e1396097b6a72939c475526
Chansung Park <deep.diver.csp@gmail.com>
2022-08-26 02:15:12 +0000

    change function call name

--- tfx_addons/firebase_publisher/runner_test.py
+++ tfx_addons/firebase_publisher/runner_test.py
@@ -15,22 +15,22 @@ class RunnerTest(tf.test.TestCase):
   def testModelExistancy(self):
     model_list = Mock()
     model_list.models = ['model1']
-    self.assertTrue(runner._model_exist(model_list))
+    self.assertTrue(runner.model_exist(model_list))
 
     model_list.models = []
-    self.assertFalse(runner._model_exist(model_list))
+    self.assertFalse(runner.model_exist(model_list))
 
   @mock.patch('tfx_addons.firebase_publisher.runner.glob.glob')
   def testModelPathAndType(self, mock_glob):
     tmp_model_path = "/tmp/saved_model"
 
     mock_glob.return_value = [f"{tmp_model_path}/model.tflite"]
-    is_tflite, model_path = runner._get_model_path_and_type(tmp_model_path)
+    is_tflite, model_path = runner.get_model_path_and_type(tmp_model_path)
     self.assertTrue(is_tflite)
     self.assertEquals(f"{tmp_model_path}/model.tflite", model_path)
 
     mock_glob.return_value = []
-    is_tflite, model_path = runner._get_model_path_and_type(tmp_model_path)
+    is_tflite, model_path = runner.get_model_path_and_type(tmp_model_path)
     self.assertFalse(is_tflite)
     self.assertEquals(tmp_model_path, model_path)
 
@@ -44,7 +44,7 @@ class RunnerTest(tf.test.TestCase):
     mock_gfile().__exit__ = Mock(return_value=False)
 
     try:
-      runner._check_model_size(mock_source)
+      runner.check_model_size(mock_source)
     except RuntimeError:
       self.fail("Runtime error occured unexpectedly")
 
@@ -52,7 +52,7 @@ class RunnerTest(tf.test.TestCase):
     mock_gfile().__enter__.return_value.size.return_value = 83886081
     mock_gfile().__exit__ = Mock(return_value=False)
     with self.assertRaises(RuntimeError):
-      runner._check_model_size(mock_source)
+      runner.check_model_size(mock_source)
 
 
 if __name__ == "__main__":
