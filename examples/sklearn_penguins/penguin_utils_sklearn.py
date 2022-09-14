1e89a9de197ce26babd0294030a896c349228a4d
Robert Crowe <robertcrowe@google.com>
2022-09-14 10:44:19 -0700

    Update penguin_utils_sklearn.py
    Lint

--- examples/sklearn_penguins/penguin_utils_sklearn.py
+++ examples/sklearn_penguins/penguin_utils_sklearn.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 # COMMENTED OUT TO AVOID CI ISSUES WHILE ARCHIVED
+# pylint: disable=pointless-string-statement
 """Python source file include Penguin pipeline functions and necessary utils.
 
 The utilities in this file are used to build a model with scikit-learn.
