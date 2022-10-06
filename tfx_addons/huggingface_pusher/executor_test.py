e5064f792ef3968f2b0963c59fe4e6a3219f76dd
Chansung Park <deep.diver.csp@gmail.com>
2022-10-06 23:08:18 +0000

    add test cases for executor

---
+++ tfx_addons/huggingface_pusher/executor_test.py
@@ -0,0 +1,71 @@
+# Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for tfx_addons.huggingface_pusher.executor."""
+
+from unittest import mock
+from unittest.mock import Mock
+
+import tensorflow as tf
+from tfx.types import standard_artifacts
+
+from tfx_addons.huggingface_pusher import executor
+
+
+class RunnerTest(tf.test.TestCase):
+  
+  @mock.patch('tfx_addons.huggingface_pusher.executor.which')
+  @mock.patch('tfx_addons.huggingface_pusher.runner.deploy_model_for_hf_hub')
+  def testCheckGitLFSInstalled(self, mock_runner_function, mock_which):
+    executor.Executor.CheckBlessing = Mock()
+    executor.Executor.GetModelPath = Mock()
+    executor.Executor._MarkPushed = Mock() # pylint: disable=protected-access
+
+    executor.Executor.CheckBlessing.return_value = True
+    executor.Executor.GetModelPath.return_value = "test_model_path"
+
+    mock_runner_function.return_value = {"test_key": "test_value", "repo_url": "test_repo_url"}
+    mock_which.return_value = "git-lfs"
+    
+    _executor = executor.Executor()
+
+    try:
+        _executor.Do({}, {
+            "pushed_model": [standard_artifacts.PushedModel()]
+        }, {})
+    except RuntimeError:
+        assert False
+
+  @mock.patch('tfx_addons.huggingface_pusher.executor.which')
+  @mock.patch('tfx_addons.huggingface_pusher.runner.deploy_model_for_hf_hub')
+  def testCheckGitLFSNotInstalled(self, mock_runner_function, mock_which):
+    executor.Executor.CheckBlessing = Mock()
+    executor.Executor.GetModelPath = Mock()
+    executor.Executor._MarkPushed = Mock() # pylint: disable=protected-access
+
+    executor.Executor.CheckBlessing.return_value = True
+    executor.Executor.GetModelPath.return_value = "test_model_path"
+
+    mock_runner_function.return_value = {"test_key": "test_value", "repo_url": "test_repo_url"}
+    mock_which.return_value = None
+    
+    _executor = executor.Executor()
+
+    try:
+        _executor.Do({}, {
+            "pushed_model": [standard_artifacts.PushedModel()]
+        }, {})
+    except RuntimeError:
+        assert True
+
