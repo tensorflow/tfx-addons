cd17b95c897111f6a0c83ab77e3bfb046726d346
Carlos Ezequiel <cezequiel@google.com>
2023-03-08 16:55:48 -0500

    pred2bq: Update component spec.

--- tfx_addons/predictions_to_bigquery/component.py
+++ tfx_addons/predictions_to_bigquery/component.py
@@ -1,23 +1,20 @@
 # Copyright 2023 The TensorFlow Authors. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
 # This code was originally written by Hannes Hapke (Digits Financial Inc.)
 # on Feb. 6, 2023.
-"""
-Digits Prediction-to-BigQuery: Functionality to write prediction results usually
- from a BulkInferrer to BigQuery.
-"""
+"""Predictions-to-bigquery component spec."""
 
 from typing import Optional
 
@@ -26,103 +23,93 @@ from tfx.dsl.components.base import base_component, executor_spec
 from tfx.types import standard_artifacts
 from tfx.types.component_spec import ChannelParameter, ExecutionParameter
 
-from .executor import Executor as AnnotateUnlabeledCategoryDataExecutor
+from tfx_addons.predictions_to_bigquery import executor
 
-_MIN_THRESHOLD = 0.8
-_VOCAB_FILE = "vocab_label_txt"
+_MIN_THRESHOLD = 0.5
 
 # pylint: disable=missing-class-docstring
 
 
-class AnnotateUnlabeledCategoryDataComponentSpec(types.ComponentSpec):
+class PredictionsToBigQueryComponentSpec(types.ComponentSpec):
 
   PARAMETERS = {
-      # These are parameters that will be passed in the call to
-      # create an instance of this component.
-      "vocab_label_file": ExecutionParameter(type=str),
-      "bq_table_name": ExecutionParameter(type=str),
-      "filter_threshold": ExecutionParameter(type=float),
-      "table_suffix": ExecutionParameter(type=str),
-      "table_partitioning": ExecutionParameter(type=bool),
-      "expiration_time_delta": ExecutionParameter(type=int),
+      'bq_table_name': ExecutionParameter(type=str),
+      'table_expiration_days': ExecutionParameter(type=int),
+      'filter_threshold': ExecutionParameter(type=float),
+      'table_partitioning': ExecutionParameter(type=bool),
+      'table_time_suffix': ExecutionParameter(type=str),
+      'vocab_label_file': ExecutionParameter(type=str),
   }
   INPUTS = {
-      # This will be a dictionary with input artifacts, including URIs
-      "transform_graph":
-      ChannelParameter(type=standard_artifacts.TransformGraph),
-      "inference_results":
-      ChannelParameter(type=standard_artifacts.InferenceResult),
-      "schema":
-      ChannelParameter(type=standard_artifacts.Schema),
+      'inference_results':
+      (ChannelParameter(type=standard_artifacts.InferenceResult)),
+      # TODO(cezequiel): Implement schema or transform_graph logic
+      'schema': (ChannelParameter(type=standard_artifacts.Schema)),
+      'transform_graph':
+      (ChannelParameter(type=standard_artifacts.TransformGraph)),
   }
   OUTPUTS = {
-      "bigquery_export": ChannelParameter(type=standard_artifacts.String),
+      'bigquery_export': ChannelParameter(type=standard_artifacts.String),
   }
 
 
-class AnnotateUnlabeledCategoryDataComponent(base_component.BaseComponent):
-  """
-    AnnotateUnlabeledCategoryData Component.
+class PredictionsToBigQueryComponent(base_component.BaseComponent):
 
-    The component takes the following input artifacts:
-    * Inference results: InferenceResult
-    * Transform graph: TransformGraph
-    * Schema: Schema (optional) if not present, the component will determine
-    the schema (only predtion supported at the moment)
-
-    The component takes the following parameters:
-    * vocab_label_file: str - The file name of the file containing the
-      vocabulary labels (produced by TFT).
-    * bq_table_name: str - The name of the BigQuery table to write the results
-      to.
-    * filter_threshold: float   - The minimum probability threshold for a
-      prediction to be considered a positive, thrustworthy prediction.
-      Default is 0.8.
-    * table_suffix: str (optional) - If provided, the generated datetime string
-      will be added the BigQuery table name as suffix. The default is %Y%m%d.
-    * table_partitioning: bool - Whether to partition the table by DAY. If True,
-      the generated BigQuery table will be partition by date. If False, no
-      partitioning will be applied. Default is True.
-    * expiration_time_delta: int (optional) - The number of seconds after which
-      the table will expire.
-
-    The component produces the following output artifacts:
-    * bigquery_export: String - The URI of the BigQuery table containing the
-      results.
-    """
-
-  SPEC_CLASS = AnnotateUnlabeledCategoryDataComponentSpec
-  EXECUTOR_SPEC = executor_spec.BeamExecutorSpec(
-      AnnotateUnlabeledCategoryDataExecutor)
+  SPEC_CLASS = PredictionsToBigQueryComponentSpec
+  EXECUTOR_SPEC = executor_spec.BeamExecutorSpec(executor.Executor)
 
   def __init__(
       self,
-      inference_results: types.Channel = None,
-      transform_graph: types.Channel = None,
-      bq_table_name: str = None,
-      vocab_label_file: str = _VOCAB_FILE,
-      filter_threshold: float = _MIN_THRESHOLD,
-      table_suffix: str = "%Y%m%d",
-      table_partitioning: bool = True,
-      schema: Optional[types.Channel] = None,
-      expiration_time_delta: Optional[int] = 0,
+      inference_results: types.Channel,
+      bq_table_name: str,
       bigquery_export: Optional[types.Channel] = None,
-  ):
+      transform_graph: Optional[types.Channel] = None,
+      schema: Optional[types.Channel] = None,
+      table_expiration_days: Optional[int] = 0,
+      filter_threshold: float = _MIN_THRESHOLD,
+      table_partitioning: bool = True,
+      table_time_suffix: str = '%Y%m%d',
+      vocab_label_file: Optional[str] = None,
+  ) -> None:
+    """Initialize the component.
 
+    Args:
+      inference_results: Inference results channel.
+      bq_table_name: BigQuery table name in either PROJECT:DATASET.TABLE.
+        or DATASET.TABLE formats.
+      bigquery_export: Outputs channel containing generated BigQuery table name.
+        The outputted name may contain a timestamp suffix defined by
+        `table_suffix`.
+      transform_graph: TFTransform graph channel.
+        If specified, and `schema` is not specified, the prediction
+        input schema shall be derived from this channel.
+      schema: Schema channel.
+        If specified, the prediction input schema shall be derived from this
+        channel.
+      expiration_days: BigQuery table expiration in number of days from
+        current time. If not specified, the table does not expire by default.
+      filter_threshold: Prediction threshold to use to filter prediction scores.
+        Keep scores that exceed this threshold.
+      table_partitioning: If True, partition table.
+        See: https://cloud.google.com/bigquery/docs/partitioned-tables
+      table_time_suffix: Time format for table suffix in Linux strftime format.
+        Example: '%Y%m%d
+      vocab_label_file: Name of the TF transform vocabulary file for the label.
+    """
     bigquery_export = bigquery_export or types.Channel(
         type=standard_artifacts.String)
-    schema = schema or types.Channel(type=standard_artifacts.Schema())
+    schema = schema or types.Channel(type=standard_artifacts.Schema)
 
-    spec = AnnotateUnlabeledCategoryDataComponentSpec(
+    spec = PredictionsToBigQueryComponentSpec(
         inference_results=inference_results,
+        bq_table_name=bq_table_name,
+        bigquery_export=bigquery_export,
         transform_graph=transform_graph,
         schema=schema,
-        bq_table_name=bq_table_name,
-        vocab_label_file=vocab_label_file,
+        table_expiration_days=table_expiration_days,
         filter_threshold=filter_threshold,
-        table_suffix=table_suffix,
         table_partitioning=table_partitioning,
-        expiration_time_delta=expiration_time_delta,
-        bigquery_export=bigquery_export,
+        table_time_suffix=table_time_suffix,
+        vocab_label_file=vocab_label_file,
     )
     super().__init__(spec=spec)
--- tfx_addons/predictions_to_bigquery/component_test.py
+++ tfx_addons/predictions_to_bigquery/component_test.py
@@ -1,52 +1,65 @@
 # Copyright 2023 The TensorFlow Authors. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
 # This code was originally written by Hannes Hapke (Digits Financial Inc.)
 # on Feb. 6, 2023.
-"""
-Tests around Digits Prediction-to-BigQuery component.
-"""
+"""Tests for component.py."""
 
-import tensorflow as tf
+import unittest
+
 from tfx.types import channel_utils, standard_artifacts
 
-from . import component
+from tfx_addons.predictions_to_bigquery import component
 
 
-class ComponentTest(tf.test.TestCase):
+class ComponentTest(unittest.TestCase):
   def setUp(self):
-    super(ComponentTest, self).setUp()
+    super().setUp()
     self._transform_graph = channel_utils.as_channel(
         [standard_artifacts.TransformGraph()])
     self._inference_results = channel_utils.as_channel(
         [standard_artifacts.InferenceResult()])
     self._schema = channel_utils.as_channel([standard_artifacts.Schema()])
 
-  def testConstruct(self):
-    # not a real test, just checking if if the component can be
-    # instantiated
-    _ = component.AnnotateUnlabeledCategoryDataComponent(
+  def testInit(self):
+    component_instance = component.PredictionsToBigQueryComponent(
         transform_graph=self._transform_graph,
         inference_results=self._inference_results,
         schema=self._schema,
-        bq_table_name="gcp_project:bq_database.table",
-        vocab_label_file="vocab_txt",
+        bq_table_name='gcp_project:bq_database.table',
+        vocab_label_file='vocab_txt',
         filter_threshold=0.1,
-        table_suffix="%Y",
         table_partitioning=False,
+        table_time_suffix='%Y%m%d',
     )
+    self.assertCountEqual({
+        'inference_results',
+        'schema',
+        'transform_graph',
+    }, component_instance.inputs.keys())
+    self.assertCountEqual({'bigquery_export'},
+                          component_instance.outputs.keys())
+    self.assertCountEqual(
+        {
+            'bq_table_name',
+            'table_expiration_days',
+            'filter_threshold',
+            'table_partitioning',
+            'table_time_suffix',
+            'vocab_label_file',
+        }, component_instance.exec_properties.keys())
 
 
-if __name__ == "__main__":
-  tf.test.main()
+if __name__ == '__main__':
+  unittest.main()
