4c6638bd62b885be48562519693f5f063cceb7c3
Valko Milev <valko.krasimirov.milev@gmail.com>
2023-04-26 18:05:02 +0300

    lint fix

--- examples/filter_component/filter_function.py
+++ examples/filter_component/filter_function.py
@@ -8,8 +8,6 @@ Returns:
   filtered list
 
 """
-# TODO: This license is not consistent with the license used in the project.
-#       Delete the inconsistent license and above line and rerun pre-commit to insert a good license.
 # Copyright 2023 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,8 +23,9 @@ Returns:
 # limitations under the License.
 # ==============================================================================
 
+
 def filter_function(x_list):
-    """Filters the data from input data by using the filter function.
+  """Filters the data from input data by using the filter function.
 
     Args:
       x_list: Input list of data to be filtered.
@@ -36,8 +35,8 @@ def filter_function(x_list):
       filtered list
 
     """
-    new_list = []
-    for element in x_list:
-        if element['label'] == [0]:
-            new_list.append(element)
-    return new_list
+  new_list = []
+  for element in x_list:
+    if element['label'] == [0]:
+      new_list.append(element)
+  return new_list
