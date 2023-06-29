395c6adc254cd590c833f4b41ae9ba9d3dcb3905
Valko Milev <valko.krasimirov.milev@gmail.com>
2023-06-29 18:43:31 +0300

    some lint fixes

--- tfx_addons/example_filter/component.py
+++ tfx_addons/example_filter/component.py
@@ -18,80 +18,79 @@ the component for filter addon
 
 import importlib
 import os
-import re
 
 import tensorflow as tf
-from google.cloud import storage
 from tfx.dsl.component.experimental.annotations import OutputDict
+from tfx.dsl.io.fileio import listdir
 from tfx.types import standard_artifacts
 from tfx.v1.dsl.components import InputArtifact, Parameter
-from tfx.dsl.io.fileio import listdir
 from tfx_bsl.coders import example_coder
 
+
 def _get_data_from_tfrecords(train_uri: str):
-    '''
-      Reads and returns data from TFRecords at URI as a list of dictionaries with values as numpy arrays
-      Example:
-        _get_data_from_tfrecords('path_to_TFRecords')
-    '''
-    train_uri = [
-        os.path.join(train_uri, file_path)
-        for file_path in listdir(train_uri)
-    ]
-    raw_dataset = tf.data.TFRecordDataset(train_uri, compression_type='GZIP')
+  '''
+        Reads and returns data from TFRecords at URI as a list
+         of dictionaries with values as numpy arrays
+        Example:
+          _get_data_from_tfrecords('path_to_TFRecords')
+      '''
+  train_uri = [
+      os.path.join(train_uri, file_path) for file_path in listdir(train_uri)
+  ]
+  raw_dataset = tf.data.TFRecordDataset(train_uri, compression_type='GZIP')
 
-    np_dataset = []
-    for tfrecord in raw_dataset:
-        serialized_example = tfrecord.numpy()
-        example = example_coder.ExampleToNumpyDict(serialized_example)
-        np_dataset.append(example)
+  np_dataset = []
+  for tfrecord in raw_dataset:
+    serialized_example = tfrecord.numpy()
+    example = example_coder.ExampleToNumpyDict(serialized_example)
+    np_dataset.append(example)
 
-    return np_dataset
+  return np_dataset
 
 
 def filter_component(input_data: InputArtifact[standard_artifacts.Examples],
                      filter_function_str: Parameter[str],
                      output_file: Parameter[str]) -> OutputDict(list_len=int):
-    """Filters the data from input data by using the filter function.
+  """Filters the data from input data by using the filter function.
 
-      Args:
-        input_data: Input list of data to be filtered.
-        output_file: the name of the file to be saved to.
-        filter_function_str: Module name of the function that will be used to
-        filter the data.
-          Example for the function
-              my_example/my_filter.py:
+        Args:
+          input_data: Input list of data to be filtered.
+          output_file: the name of the file to be saved to.
+          filter_function_str: Module name of the function that will be used to
+          filter the data.
+            Example for the function
+                my_example/my_filter.py:
 
-              # filter module must have filter_function implemented
-              def filter_function(input_list: Array):
-                  output_list = []
-                  for element in input_list:
-                      if element.something:
-                          output_list.append(element)
-                  return output_list
+                # filter module must have filter_function implemented
+                def filter_function(input_list: Array):
+                    output_list = []
+                    for element in input_list:
+                        if element.something:
+                            output_list.append(element)
+                    return output_list
 
-              pipeline.py:
-              filter_component(input_data ,'my_example.my_filter',output_data)
+                pipeline.py:
+                filter_component(input_data ,'my_example.my_filter',output_data)
 
-      Returns:
-        len of the list after the filter
-             {
-               'list_len': len(output_list)
-             }
+        Returns:
+          len of the list after the filter
+               {
+                 'list_len': len(output_list)
+               }
 
-      """
-    records = _get_data_from_tfrecords(input_data.uri + "/Split-train")
-    filter_function = importlib.import_module(
-        filter_function_str).filter_function
-    filtered_data = filter_function(records)
-    result_len = len(filtered_data)
-    new_data = []
-    for key in list(filtered_data[0].keys()):
-        local_list = []
-        for i in range(result_len):
-            local_list.append(str(filtered_data[i][key][0]))
-        new_data.append(str(local_list))
-    writer = tf.io.TFRecordWriter(output_file)
-    writer.write(tf.data.Dataset.from_tensor_slices(new_data).map(lambda x: x))
+        """
+  records = _get_data_from_tfrecords(input_data.uri + "/Split-train")
+  filter_function = importlib.import_module(
+      filter_function_str).filter_function
+  filtered_data = filter_function(records)
+  result_len = len(filtered_data)
+  new_data = []
+  for key in list(filtered_data[0].keys()):
+    local_list = []
+    for i in range(result_len):
+      local_list.append(str(filtered_data[i][key][0]))
+    new_data.append(str(local_list))
+  writer = tf.io.TFRecordWriter(output_file)
+  writer.write(tf.data.Dataset.from_tensor_slices(new_data).map(lambda x: x))
 
-    return {'list_len': result_len}
+  return {'list_len': result_len}
