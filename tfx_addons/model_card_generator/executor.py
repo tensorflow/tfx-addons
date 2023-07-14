ec6065bb5f505dbd11b9935b5b1d4186f194ae51
Suzen Fylke <codesue@users.noreply.github.com>
2023-07-14 14:45:51 -0400

    Surface feature and metric include/exclude options

--- tfx_addons/model_card_generator/executor.py
+++ tfx_addons/model_card_generator/executor.py
@@ -33,23 +33,35 @@ class Executor(BaseExecutor):
   """Executor for Model Card TFX component."""
   def _tfma_source(
       self,
-      input_dict: Dict[str, List[types.Artifact]]) -> Optional[src.TfmaSource]:
+      input_dict: Dict[str, List[types.Artifact]],
+      exec_properties: Dict[str, Any],
+  ) -> Optional[src.TfmaSource]:
     """See base class."""
     if not input_dict.get(standard_component_specs.EVALUATION_KEY):
       return None
     else:
-      return src.TfmaSource(model_evaluation_artifacts=input_dict[
-          standard_component_specs.EVALUATION_KEY])
+      return src.TfmaSource(
+          model_evaluation_artifacts=input_dict[
+              standard_component_specs.EVALUATION_KEY],
+          metrics_include=exec_properties.get('metrics_include', []),
+          metrics_exclude=exec_properties.get('metrics_exclude', []),
+      )
 
   def _tfdv_source(
       self,
-      input_dict: Dict[str, List[types.Artifact]]) -> Optional[src.TfdvSource]:
+      input_dict: Dict[str, List[types.Artifact]],
+      exec_properties: Dict[str, Any],
+  ) -> Optional[src.TfdvSource]:
     """See base class."""
     if not input_dict.get(standard_component_specs.STATISTICS_KEY):
       return None
     else:
-      return src.TfdvSource(example_statistics_artifacts=input_dict[
-          standard_component_specs.STATISTICS_KEY])
+      return src.TfdvSource(
+          example_statistics_artifacts=input_dict[
+              standard_component_specs.STATISTICS_KEY],
+          features_include=exec_properties.get('features_include', []),
+          features_exclude=exec_properties.get('features_exclude', []),
+      )
 
   def _model_source(
       self,
@@ -100,13 +112,28 @@ class Executor(BaseExecutor):
           `ModelCardToolkit`'s default HTML template
           (`default_template.html.jinja`) and file name (`model_card.html`)
           are used.
+        - features_include: The feature paths to include for the dataset
+          statistics.
+          By default, all features are included. Mutually exclusive with
+          features_exclude.
+        - features_exclude: The feature paths to exclude for the dataset
+          statistics.
+          By default, all features are included. Mutually exclusive with
+          features_include.
+        - metrics_include: The list of metric names to include in the model
+          card. By default, all metrics are included. Mutually exclusive with
+          metrics_exclude.
+        - metrics_exclude: The list of metric names to exclude in the model
+          card. By default, no metrics are excluded. Mutually exclusive with
+          metrics_include.
     """
 
     # Initialize ModelCardToolkit
     mct = core.ModelCardToolkit(source=src.Source(
-        tfma=self._tfma_source(input_dict),
-        tfdv=self._tfdv_source(input_dict),
-        model=self._model_source(input_dict)),
+        tfma=self._tfma_source(input_dict, exec_properties),
+        tfdv=self._tfdv_source(input_dict, exec_properties),
+        model=self._model_source(input_dict),
+    ),
                                 output_dir=artifact_utils.get_single_instance(
                                     output_dict['model_card']).uri)
     template_io = exec_properties.get('template_io') or [
